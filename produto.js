fafcd4fdde0282e6e8e5c01482bb2533a7c312ab
EmyyS2 <miacontapcsenai@gmail.com>
2023-08-24 12:22:17 -0300

    atualização produtos

--- produto.js
+++ produto.js
@@ -8,7 +8,7 @@ class Produto {
         return "Produto " + this.nome + " por R$" + this.preco + " Reais";
     }
     vejaDesconto(desconto) {
-        console.log("Desconto de " + desconto + "% aplicado. Novo preço R$ " + (this.preco = this.preco * desconto / 100));
+        return "Desconto de " + desconto + "% aplicado. Novo preço R$ " + (this.preco = this.preco * desconto / 100);
     }
 }
 let produto1 = new Produto("Hello Kitty", 100);
@@ -16,7 +16,9 @@ let produto2 = new Produto("My melody", 150);
 let produto3 = new Produto("Kuromi", 200);
 console.log(produto1.vejaProduto());
 console.log(produto1.vejaDesconto(50));
+console.log();
 console.log(produto2.vejaProduto());
 console.log(produto2.vejaDesconto(75));
+console.log();
 console.log(produto3.vejaProduto());
 console.log(produto3.vejaDesconto(50));
--- produto.ts
+++ produto.ts
@@ -10,7 +10,7 @@ class Produto {
         return "Produto " + this.nome + " por R$" + this.preco + " Reais";
     }
     vejaDesconto(desconto: number) {
-        console.log("Desconto de " + desconto + "% aplicado. Novo preço R$ " +( this.preco=this.preco * desconto/100));
+       return "Desconto de " + desconto + "% aplicado. Novo preço R$ " +( this.preco=this.preco * desconto/100);
        
     }
 }
@@ -20,9 +20,9 @@ let produto3 = new Produto("Kuromi", 200,)
 
 console.log(produto1.vejaProduto());
 console.log(produto1.vejaDesconto(50));
-
+console.log();
 console.log(produto2.vejaProduto());
 console.log(produto2.vejaDesconto(75));
-
+console.log();
 console.log(produto3.vejaProduto());
 console.log(produto3.vejaDesconto(50));
